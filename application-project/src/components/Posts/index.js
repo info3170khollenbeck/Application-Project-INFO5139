66eb59cb6affbb4672ac47573f8ef84e1c1788ed
Keiran Hollenbeck <kmankeiran@gmail.com>
2023-03-04 13:46:16 -0500

    broken usestate so far

--- application-project/src/components/Posts/index.js
+++ application-project/src/components/Posts/index.js
@@ -1,9 +1,24 @@
 import Post from "./Post";
+import { useEffect, useState } from 'react';
 
 export default function Posts({ posts }) {
+    const [postListTwo, setPostListTwo] = useState([])  
+    useEffect(() => {
+        let postList = [];
+
+        for (let i = 0; i < 10; i++) {
+            var postIndex = Math.floor(Math.random() * (posts.length - 1)); 
+            postList += posts[postIndex];
+            console.log(postList);
+        }
+        setPostListTwo(postList);
+    });
+        
+    
+    
     return (
         <main>
-            {posts.map((post, index) => (
+            {postListTwo.map((post, index) => (
                 <Post
                     {...post}
                 />
