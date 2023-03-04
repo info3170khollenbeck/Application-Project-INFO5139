c19b145cb04b44e7098f97b34a1209f5240d6745
Jamie Antonucci <j_antonucci@fanshaweonline.ca>
2023-03-04 13:33:57 -0500

    added sign up button

--- application-project/src/App.js
+++ application-project/src/App.js
@@ -1,12 +1,19 @@
 import Posts from "./components/Posts";
 import Header from "./components/Header";
 import samplePosts from "./content/posts";
+import {useState} from 'react';
 
 function App() {
   const posts = samplePosts;
+  const [showSignin, setShowSignin] = useState(false);
+
   return (
     <>
-      <Header />
+      {showSignin&& (
+        "Sign In Component"
+      )
+      }
+      <Header signin={showSignin} setShowSignin={setShowSignin}/>
       <Posts posts={posts} />
     </>
   );
--- application-project/src/components/Header/index.js
+++ application-project/src/components/Header/index.js
@@ -1,12 +1,16 @@
 import "./styles.scss";
 
-export default function Header() {
+export default function Header({ signin, setShowSignin }) {
+  const handleSignin = () => {
+    signin ? setShowSignin(false) : setShowSignin(true);
+  };
+
   return (
     <div className="header-component">
       <h1>BloomScroll</h1>
-      <div className="signup-button">
-            Sign Up
-      </div>
+      <button onClick={() => handleSignin(true)} className="signup-button">
+        Sign Up
+      </button>
     </div>
   );
 }
