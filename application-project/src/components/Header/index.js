55c842ab9c230335190bf8f64e408bec1716c530
Jamie Antonucci <j_antonucci@fanshaweonline.ca>
2023-03-21 17:42:26 -0400

    hiding signin fields until button is pressed

--- application-project/src/components/Header/index.js
+++ application-project/src/components/Header/index.js
@@ -6,96 +6,106 @@ import { createUserWithEmailAndPassword } from '@firebase/auth';
 import { signInWithEmailAndPassword } from '@firebase/auth';
 
 export default function Header({ setShowSignin }) {
-  const [error, setError] = useState('');
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [userEmail, setUserEmail] = useState('');
-  const emailRef = useRef();
-  const passwordRef = useRef();
+	const [error, setError] = useState('');
+	const [loggedIn, setLoggedIn] = useState(false);
+	const [userEmail, setUserEmail] = useState('');
+	const [showLoginFields, setShowLoginFields] = useState(false);
+	const emailRef = useRef();
+	const passwordRef = useRef();
 
-  async function handleSignUp(e) {
-    e.preventDefault();
-    try {
-      await createUserWithEmailAndPassword(
-        auth,
-        emailRef.current.value,
-        passwordRef.current.value
-      );
-      setError('');
-      setShowSignin(false);
-      setLoggedIn(true);
-      setUserEmail(emailRef.current.value);
-      console.log('User created!', emailRef.current.value);
-    } catch (error) {
-      setError('Failed to create an account');
+	async function handleSignUp(e) {
+		e.preventDefault();
+		try {
+			await createUserWithEmailAndPassword(
+				auth,
+				emailRef.current.value,
+				passwordRef.current.value
+			);
+			setError('');
+			setShowSignin(false);
+			setLoggedIn(true);
+			setUserEmail(emailRef.current.value);
+			console.log('User created!', emailRef.current.value);
+		} catch (error) {
+			setError('Failed to create an account');
+		}
+	}
+
+	async function handleSignIn(e) {
+		e.preventDefault();
+		try {
+			await signInWithEmailAndPassword(
+				auth,
+				emailRef.current.value,
+				passwordRef.current.value
+			);
+			setError('');
+			setShowSignin(false);
+			setLoggedIn(true);
+			setUserEmail(emailRef.current.value);
+			console.log('User logged in!', emailRef.current.value);
+		} catch (error) {
+			setError('Failed to sign in');
+		}
+	}
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		if (showLoginFields) {
+			if (e.target.name === 'signup') {
+				await handleSignUp(e);
+				console.log('signup handled');
+			} else if (e.target.name === 'signin') {
+				await handleSignIn(e);
+				console.log('signin handled');
+			}
+		} else {
+      setShowLoginFields(true);
     }
-  }
+	}
 
-  async function handleSignIn(e) {
-    e.preventDefault();
-    try {
-      await signInWithEmailAndPassword(
-        auth,
-        emailRef.current.value,
-        passwordRef.current.value
-      );
-      setError('');
-      setShowSignin(false);
-      setLoggedIn(true);
-      setUserEmail(emailRef.current.value);
-      console.log('User logged in!', emailRef.current.value);
-    } catch (error) {
-      setError('Failed to sign in');
-    }
-  }
+	return (
+		<>
+			<div className='header-component'>
+				<h1>BloomScroll</h1>
+				{loggedIn ? (
+					<p>Welcome and Enjoy! {userEmail}!</p>
+				) : (
+					<Form>
+						{showLoginFields && (
+							<>
+								<input
+									type='email'
+									className='input-element'
+									placeholder='Email'
+									ref={emailRef}
+								/>
+								<input
+									type='password'
+									className='input-element'
+									placeholder='Password'
+									ref={passwordRef}
+								/>
+							</>
+						)}
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    if (e.target.name === 'signup') {
-      await handleSignUp(e);
-      console.log('signup handled');
-    } else if (e.target.name === 'signin') {
-      await handleSignIn(e);
-      console.log('signin handled');
-    }
-  }
-
-  return (
-    <>
-      <div className='header-component'>
-        <h1>BloomScroll</h1>
-        {loggedIn ? (
-          <p>Welcome and Enjoy! {userEmail}!</p>
-        ) : (
-          <Form>
-            <input
-              type='email'
-              className='input-element'
-              placeholder='Email'
-              ref={emailRef}
-            />
-            <input
-              type='password'
-              className='input-element'
-              placeholder='Password'
-              ref={passwordRef}
-            />
-            <button
-              name='signup'
-              onClick={handleSubmit}
-              className='signup-button'
-            >
-              Sign Up
-            </button>
-            <button
-              name='signin'
-              onClick={handleSubmit}
-              className='signup-button'
-            >
-              Sign In
-            </button>
-          </Form>
-        )}
-      </div>
-    </>
-  );
+						<button
+							name='signup'
+							onClick={handleSubmit}
+							className='signup-button'
+						>
+							Sign Up
+						</button>
+						<button
+							name='signin'
+							onClick={handleSubmit}
+							className='signup-button'
+						>
+							Sign In
+						</button>
+					</Form>
+				)}
+			</div>
+		</>
+	);
 }
