345ac31b0c2d006d41cf6c45b26ae836e674524c
Keiran Hollenbeck <kmankeiran@gmail.com>
2023-03-21 18:24:40 -0400

    created like button with simple functions

--- application-project/src/components/Posts/Post/index.js
+++ application-project/src/components/Posts/Post/index.js
@@ -1,21 +1,23 @@
 import './styles.scss';
+import { BsFillSuitHeartFill } from 'react-icons/bs'
+
+const likePost = () => {
+  console.log("Hello there Like");
+}
 
 export default function Post({ title, type, img, body, source }) {
   return (
-    <div className={type}>
-      {title && <h3>{title}</h3>}
-      {body && <p>{body}</p>}
-      {img && <img className='post-img' src={img} alt={title} />}
-      {source && (
-        <div className='source-field'>
-          <strong>Source: </strong>
-          <a href={source}>{source}</a>
-        </div>
-      )}
-      <div className='like-field'>
-        <button>Like</button>
-        <span>likes</span>
+      <div className={type}>
+        {title && <h3>{title}</h3>}
+        {body && <p>{body}</p>}
+        {img && <img className="post-img" src={img} alt={title} />}
+        {source && (
+          <div className="source-field">
+            <strong>Source: </strong>
+            <a href={source}>{source}</a>
+          </div>
+        )}
+        <button className="likeButton" onClick={likePost}><BsFillSuitHeartFill size={30}/></button>
       </div>
-    </div>
   );
 }
