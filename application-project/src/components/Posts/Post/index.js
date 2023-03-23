d7711f7866ebfdb3d9a8bc5b685345da973c63f5
Keiran Hollenbeck <kmankeiran@gmail.com>
2023-03-23 17:09:48 -0400

    put the buttons in a div to organize, created the hide post button and gave it functionality

--- application-project/src/components/Posts/Post/index.js
+++ application-project/src/components/Posts/Post/index.js
@@ -1,14 +1,25 @@
 import "./styles.scss";
 import "../../../styles/variables.scss";
-import { BsFillSuitHeartFill } from 'react-icons/bs'
+import { BsFillSuitHeartFill } from 'react-icons/bs';
+import { BiHide } from 'react-icons/bi';
+import { useState } from 'react';
 
-const likePost = (event) => {
-  console.log("Hello there Like");
-  event.target.style.color = "#b57ba6";
-}
 
 export default function Post({ title, type, img, body, source }) {
-  return (
+  const [isHidden, setIsHidden] = useState(true);
+
+  const likePost = (event) => {
+    console.log("Hello there Like");
+    event.target.style.color = "#b57ba6";
+  }
+
+  const hidePost = () => {
+    console.log("The content is now hidden");
+    setIsHidden(false);
+  }
+
+
+  return isHidden ? (
       <div className={type}>
         {title && <h3>{title}</h3>}
         {body && <p>{body}</p>}
@@ -19,7 +30,10 @@ export default function Post({ title, type, img, body, source }) {
             <a href={source}>{source}</a>
           </div>
         )}
-        <button className="likeButton" onClick={likePost}><BsFillSuitHeartFill size={30}/></button>
+        <div className="buttonDiv">
+          <button className="likeButton" onClick={likePost}><BsFillSuitHeartFill size={30}/></button>
+          <button className="hideButton" onClick={hidePost}><BiHide size={30}/></button>
+        </div>
       </div>
-  );
+  ) : null;
 }
